0df47722cc62cfe570874320e40a8933bd258bfb
Anas Ansari <anasansari056789@gmail.com>
2026-02-21 14:42:49 +0530

    add user hook and isAuthorized

---
+++ hooks/useUser.ts
@@ -0,0 +1,18 @@
+'use client'
+
+import {isAuthorized} from '@/lib/isAuthorized'
+import { useEffect, useState } from 'react';
+
+export const useUser = async () => {
+  const [email, setEmail] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    const fetchUser= async ()=> {
+      const user=await isAuthorized();
+      setEmail(user.email);
+      setLoading(false);
+    }
+    fetchUser();
+  },[]); 
+}

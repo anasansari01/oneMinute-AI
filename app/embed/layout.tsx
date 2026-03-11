fcf6f811eafbbef672cccb26ed5541865b338308
Anas Ansari <anasansari056789@gmail.com>
2026-03-11 15:08:44 +0530

    add embed page to application

---
+++ app/embed/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmbedPageLayout = ({children}: {children:React.ReactNode}) => {
+  return (
+    <div className={`bg-[#050509] min-h-screen flex flex-col p-0 antialiased text-zinc-100 selection:bg-zinc-800 font-sans`}>
+      {children}
+    </div>
+  )
+}
+
+export default EmbedPageLayout
